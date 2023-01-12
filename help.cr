4f5275f98e989a94f95d608bca79bee878e57b9b
Edmilson Ferreira da Silva <edmilsonefs@users.noreply.github.com>
2023-01-11 23:43:47 -0300

    Add 1 flag to get the filename

--- help.cr
+++ help.cr
@@ -1,5 +1,7 @@
 require "option_parser"
 
+filename = ""
+
 OptionParser.parse do |parser|
   parser.banner = "Welcome to The PO 2Invoice CLI App!"
 
@@ -10,6 +12,9 @@ OptionParser.parse do |parser|
   parser.on "-h", "--help", "Show help" do
     puts parser
     exit
+  end
+  parser.on "-f", "--file FILENAME", "Specify the PO file to convert" do |value|
+    filename = value
   end
   parser.missing_option do |option_flag|
     STDERR.puts "ERROR: #{option_flag} is missing something."
@@ -22,4 +27,9 @@ OptionParser.parse do |parser|
     STDERR.puts parser
     exit(1)
   end
+end
+
+unless filename.empty?
+  STDERR.puts ""
+  puts "Converting #{filename} to Invoice..."
 end
